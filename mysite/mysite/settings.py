0597c10b68469fd93618500b37b6adbbddf616f3
Micah Nissly <mnissly@gmail.com>
2020-05-08 23:08:59 -0400

    more cleaning to make heroku work

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,6 +122,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 import django_heroku
 django_heroku.settings(locals())
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'bridge/static'),
-)
