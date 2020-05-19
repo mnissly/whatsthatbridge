c16c000d5d800c7bd3eda040866f7c30fe556006
Micah Nissly <mnissly@gmail.com>
2020-05-19 19:38:18 -0400

    added javascript from https://www.w3schools.com/html/html5_geolocation.asp to get and show user location

--- mysite/bridge/models.py
+++ mysite/bridge/models.py
@@ -6,8 +6,8 @@ class Bridge(models.Model):
     description = models.CharField(max_length=600, default="this bridge has no description")
     year_built = models.CharField(max_length=600, default="unknown year")
     # purpose of defaults is to make sure if bridge location is unknown, app doesn't find it as the closest to someone.
-    lat = models.FloatField(default=-32.6536) # default lat is that of Mt. Aconcogua, highest peak in South America
-    long = models.FloatField(default=-70.0115) # default long is that of Mt. Aconcogua, highest peak in South America
+    lat = models.FloatField(default=-32.6536)  # default lat is that of Mt. Aconcogua, highest peak in South America
+    long = models.FloatField(default=-70.0115)  # default long is that of Mt. Aconcogua, highest peak in South America
     picture = models.CharField(max_length=600, null=True, blank=True)
 
 
