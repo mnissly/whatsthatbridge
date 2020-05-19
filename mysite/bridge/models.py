d340729514004620df48c7578cb172fd8175487e
Micah Nissly <mnissly@gmail.com>
2020-05-19 19:04:59 -0400

    added lat and long fields to Bridge model for location-based features.

--- mysite/bridge/models.py
+++ mysite/bridge/models.py
@@ -5,6 +5,8 @@ class Bridge(models.Model):
     name = models.CharField(max_length=600)
     description = models.CharField(max_length=600, default="this bridge has no description")
     year_built = models.CharField(max_length=600, default="unknown year")
+    lat = models.FloatField(default=-32.6536)
+    long = models.FloatField(default=-70.0115)
     picture = models.CharField(max_length=600, null=True, blank=True)
 
 
