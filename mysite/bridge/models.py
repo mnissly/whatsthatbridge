1c76d51bd49b6c8de2e935041355124ac7cef7a2
Micah Nissly <mnissly@gmail.com>
2020-05-19 19:06:44 -0400

    commented lat and long fields ;)

--- mysite/bridge/models.py
+++ mysite/bridge/models.py
@@ -5,8 +5,8 @@ class Bridge(models.Model):
     name = models.CharField(max_length=600)
     description = models.CharField(max_length=600, default="this bridge has no description")
     year_built = models.CharField(max_length=600, default="unknown year")
-    lat = models.FloatField(default=-32.6536)
-    long = models.FloatField(default=-70.0115)
+    lat = models.FloatField(default=-32.6536) # default lat is that of Mt. Aconcogua, highest peak in South America
+    long = models.FloatField(default=-70.0115) # default long is that of Mt. Aconcogua, highest peak in South America
     picture = models.CharField(max_length=600, null=True, blank=True)
 
 
