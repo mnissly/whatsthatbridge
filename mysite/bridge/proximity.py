6abfb1a79867c673929231559da9b0a16652dd5a
Micah Nissly <mnissly@gmail.com>
2020-05-20 13:32:05 -0400

    created proximity.py for finding closest bridge

---
+++ mysite/bridge/proximity.py
@@ -0,0 +1,14 @@
+import geopy.distance
+
+from .models import Bridge
+
+
+def nearest_bridge(lat, long):
+    """
+    this function will open the page and throw out the entire contents of the page
+    :param lat: latitude of user
+    :param long: longitude of user
+    :return: closest bridge
+    """
+    bridge_id = 1
+    return bridge_id
